ec5346a9c103d6bee3252b7c9b5afdd438e8f711
Alexander Demidionok <ADemidionok1@protectiveinsurance.com>
2024-12-18 22:13:32 -0500

    Ура, я инициализировал переменную

--- Module_3_Ex_3.2.4/Program.cs
+++ Module_3_Ex_3.2.4/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        string MyName = "Jane";
+
+        Console.WriteLine(MyName);
+
+        Console.ReadKey();
+    }
+}
