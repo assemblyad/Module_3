3f454dd31322468ac1cf118bc4c217474ff065f7
Alexander Demidionok <ADemidionok1@protectiveinsurance.com>
2024-12-19 16:05:45 -0500

    unar and binar oparations

--- Module_3_Ex_3.2.4/Program.cs
+++ Module_3_Ex_3.2.4/Program.cs
@@ -4,13 +4,14 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-
+        /*
         const string MyName = "Jane";
 
-        Console.WriteLine(MyName);
+        Console.WriteLine($"My name is {MyName}");
 
-        Console.WriteLine("Hello,\n world");
+        Console.WriteLine("\tHello,\n world");
         Console.WriteLine("\t I am 45 years old");
+        Console.WriteLine();
         Console.WriteLine("\t My name is Alexander");
         Console.WriteLine("\u0040");
         Console.WriteLine("\x23");
@@ -28,7 +29,67 @@ class MainClass
         Console.WriteLine("Do I have a pet? " + HaveIApet);
         Console.WriteLine("My shoe size is " + MyShoeSize);
 
+        DaysOfWeek MyFavoriteDay;
+
+        MyFavoriteDay = DaysOfWeek.Friday;
+
+        Console.WriteLine(MyFavoriteDay);
+
+        Semaphore SemaphoreColor;
+
+        SemaphoreColor = Semaphore.Green;
+
+        Console.WriteLine(SemaphoreColor);
+
+
+        string MyName1 = "Alexander";
+        Console.WriteLine("Hello, world");
+        Console.WriteLine();
+        Console.WriteLine("My name {0}", MyName1);
+
+        //0 — позиция нашей переменной. То есть переменная будет на позиции 0.
+
+
+        //Также мы можем вставить в строку не переменную, а строковый литерал:
+        Console.WriteLine("My name {0}", "Alexander New");
+
+
+        //И, естественно, мы можем использовать несколько переменных при выводе. В этом случае располагаем их в порядке нумерации «мест».
+        var age = 27;
+        var weight = 50;
+        Console.WriteLine("Age {0} \n weight {1}", age, weight);
+
+
+        string MyName2 = "Computer";
+        Console.WriteLine("Hello, human");
+        Console.WriteLine("My name is {0}", MyName2);
+        Console.Write("What is your name? ");
+        string YourName = Console.ReadLine();
+        Console.Write("Happy to meet you, {0} ", YourName);
+        */
+
+        double result = 5.0 / 2.0 ;
+        Console.WriteLine("5 / 2 = {0}", result);
+
+        double result1 = 5.0 % 2.0;
+        Console.WriteLine("5 % 2 = {0}", result1);
 
         Console.ReadKey();
     }
+}
+
+
+enum DaysOfWeek : byte
+{
+    Tuesday,
+    Monday,
+    Wednesday,
+    Friday
+}
+
+enum Semaphore 
+{
+    Red = 100,
+    Yellow = 200,
+    Green = 300
 }
