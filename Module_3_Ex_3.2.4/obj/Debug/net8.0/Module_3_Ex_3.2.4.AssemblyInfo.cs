f240f09774c59c50ae623921044127f4cd471547
Alexander Demidionok <ADemidionok1@protectiveinsurance.com>
2024-12-28 20:22:43 -0500

    Last changes to the Module_3_ex_3.2.4

--- Module_3_Ex_3.2.4/obj/Debug/net8.0/Module_3_Ex_3.2.4.AssemblyInfo.cs
+++ Module_3_Ex_3.2.4/obj/Debug/net8.0/Module_3_Ex_3.2.4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module_3_Ex_3.2.4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cacb165214334c89412aea904d3f6b741bbe1655")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e5a6f901f212de33c500a264476eadc75b53103")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module_3_Ex_3.2.4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module_3_Ex_3.2.4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Module_3_Ex_3.2.4/obj/Release/net8.0/Module_3_Ex_3.2.4.AssemblyInfo.cs
+++ Module_3_Ex_3.2.4/obj/Release/net8.0/Module_3_Ex_3.2.4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module_3_Ex_3.2.4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a4c8d119c5eba44c0d0f12ca8a6042a96136d38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e5a6f901f212de33c500a264476eadc75b53103")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module_3_Ex_3.2.4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module_3_Ex_3.2.4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
